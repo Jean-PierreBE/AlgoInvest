ae35fe49a33cd5b38dfa5c316f19965e2706c1ca
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-02-22 21:40:57 +0100

    deuxième version

--- optimized.py
+++ optimized.py
@@ -13,12 +13,12 @@ for key, value in DATA_FILES.items():
 
     df_data['gain'] = df_data['price']*df_data['profit']/100
     """create array"""
-    names = df_data['name'].to_list()
-    prices = df_data['price'].to_list()
-    profits = df_data['profit'].to_list()
-    gains = df_data['gain'].to_list()
+    noms_action = df_data['name'].to_list()
+    prix = df_data['price'].to_list()
+    taux_profits = df_data['profit'].to_list()
+    montant_profits = df_data['gain'].to_list()
 
-    OptimalRepartition = getValeurMax(names, prices, profits,
-                                      gains,  max_price)
+    OptimalRepartition = getValeurMax(noms_action, prix, taux_profits,
+                                      montant_profits,  max_price)
 
     print_result(OptimalRepartition, TITLE_RESULTS.format(key))
